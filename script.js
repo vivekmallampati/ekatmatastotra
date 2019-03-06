2e9ef827b8d41525be0824d4ada36084d1820d2a
Administrator <admin@wirelessprv-10-195-232-108.near.illinois.edu>
2019-03-05 23:35:00 -0600

    Working on integrating the interactive page into the play page.

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ function getHighlighted(){
   let start = window.getSelection().baseOffset;
   let end = window.getSelection().extentOffset;
   if(end - start < 2){
-    return;
+    return a;
   }
   let baseText = ourSelection.extentNode.data;
   let selection = baseText.substring(start,end);
-  alert(selection);
+  return selection;
 
 }
+function enableisOn(){
+  isOn = true;
+}
 
+function disableisOn(){
+  isOn = false;
+}
+
+function search(){
+  let query = getHighlighted();
+  if(query == 'a'){
+    return;
+  }
+  url = 'http://www.google.com/search?q=' + query;
+  window.open(url,'_blank');
+}
 // for (var i = 0; i < 1000; i++) {
 //   getHighlighted()
 // }
 window.onmouseup = function(){
-  getHighlighted();
+  if (isOn = true) {
+    search();
+  }
 }
 // let count = 0;
 // window.setInterval(function(){
