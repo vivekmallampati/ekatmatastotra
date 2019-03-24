c4fea574a52ee1063101e3d46f5489b01ae5a4fd
Vivek Mallampati <32147718+vivekmallampati@users.noreply.github.com>
2019-03-24 10:30:39 -0500

    autoscroll feature added

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ function blackPrevSelection(){
     var presanskrithSelection = document.getElementById(preSverse);
     var preenglishSelection = document.getElementById(preEverse);
     var premeaningSelection = document.getElementById(preMeaning);
+    premeaningSelection.scrollIntoView();
     premeaningSelection.style.color = 'Black';
     presanskrithSelection.style.color = 'Black';  //CORNSILK
     preenglishSelection.style.color = 'Black';  //CORNSILK
